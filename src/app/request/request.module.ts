cb3ca9768a2eebc5b5f9ffb3bda98ddbcae68c8f
Luis Leopardi <leopardi.luis@gmail.com>
2019-06-10 22:21:35 -0500

    feat(request): agrega modulo de request

---
+++ src/app/request/request.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RequestRoutingModule } from './request-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RequestRoutingModule
+  ]
+})
+export class RequestModule { }
